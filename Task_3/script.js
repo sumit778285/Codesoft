4c352578c3496920b05658003a3c49137faac130
sumit778285 <sjha15305@gmail.com>
2024-09-27 10:14:50 +0530

    Add files via upload

---
+++ Task_3/script.js
@@ -0,0 +1,27 @@
+function appendNumber(number) {
+    const display = document.getElementById('display');
+    display.value += number;
+}
+
+function appendOperator(operator) {
+    const display = document.getElementById('display');
+    display.value += ' ' + operator + ' ';
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function deleteLast() {
+    const display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
